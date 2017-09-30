82826fbfec80fa7828dd0366b1fd68fac38b67eb
unknown <deksss@i.ua>
2017-09-30 14:50:51 +0300

    app42 integrate

--- src/index.js
+++ src/index.js
@@ -3,8 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './Containers/App';
 import registerServiceWorker from './registerServiceWorker';
-import {test} from "./Api/twitter"
+import {test} from "./Api/twitter";
+import {pushData, getAllData} from "./Api/app42";
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
 test();
+pushData({name: 'React' + ~~(Math.random()*10), type: 'publicHashtag'});
+getAllData();
